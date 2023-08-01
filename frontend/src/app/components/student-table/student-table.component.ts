c027b3fcdac9d54a4864acab98d854f3d6afefe2
Hiruni Thaksarani <hthaksarani@gmail.com>
2023-08-01 21:54:36 +0530

    front

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,NavigationExtras } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router';
 import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
-import {AppServiceService} from '../../app-service.service';
+import { AppServiceService } from '../../app-service.service';
 
 @Component({
   selector: 'app-student-table',
@@ -16,38 +16,47 @@ export class StudentTableComponent implements OnInit {
   studentData: any;
   selected: any;
 
-  constructor(private service : AppServiceService, private router: Router) { }
+  constructor(private service: AppServiceService, private router: Router) { }
 
   ngOnInit(): void {
     this.getStudentData();
   }
 
-  addNewStudent(){
+  addNewStudent() {
     this.router.navigate(['addStudent'])
   }
 
-  editStudent(id){
+  editStudent(id) {
     const navigationExtras: NavigationExtras = {
       state: {
-        id : id
+        id: id
       }
     };
-    this.router.navigate(['editStudent'], navigationExtras )
+    this.router.navigate(['editStudent'], navigationExtras)
   }
 
-  getStudentData(){
-    this.service.getStudentData().subscribe((response)=>{
-      this.studentData = Object.keys(response).map((key) => [response[key]]);
-    },(error)=>{
+  initializeDB(){
+    this.service.initializeDB().subscribe((response) => {
+      console.log('DB is Initialized')
+    }, (error) => {
       console.log('ERROR - ', error)
     })
   }
 
-  deleteStudent(id){
+  getStudentData() {
+    this.selected = 'Students';
+    this.service.getStudentData().subscribe((response) => {
+      this.studentData = Object.keys(response).map((key) => [response[key]]);
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
+  deleteStudent(itemid) {
     const student = {
-      id: id
+      id: itemid
     }
-    this.service.deleteStudent(student).subscribe((response)=>{
+    this.service.deleteStudent(student).subscribe((response) => {
       this.getStudentData()
     })
   }
